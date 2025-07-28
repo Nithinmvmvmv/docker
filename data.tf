da0eca9d9a63e34bcc96ff4e77f312f3379d953a
Nithinmv <nithin8964@gmail.com>
2025-07-28 14:29:44 +0530

    t3-medium

--- data.tf
+++ data.tf
@@ -1,28 +1,19 @@
-#!/bin/bash
+data "aws_ami" "joindevops" {
+  owners           = ["973714476881"]
+  most_recent      = true
 
-dnf -y install dnf-plugins-core
-dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
 
-growpart /dev/nvme0n1 4
-lvextend -L +20G /dev/RootVG/rootVol
-lvextend -L +10G /dev/RootVG/varVol
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 
-xfs_growfs /
-xfs_growfs /var
-
-ARCH=amd64
-PLATFORM=$(uname -s)_$ARCH
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
-
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mv kubectl /usr/local/bin/kubectl
-
-eksctl version
-kubectl version
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
